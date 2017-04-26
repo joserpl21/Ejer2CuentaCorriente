720e68b0e773a85fd447472b114f16d1e56976be
Alumno <Alumno@INF713>
2017-04-26 14:11:01 +0200

    CuentaCorriete Ejer2 2

--- src/cuentacorriente/Main.java
+++ src/cuentacorriente/Main.java
@@ -10,13 +10,13 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        CuentaCorriente miCuenta;
+        CuentaCorriente cuenta1;
         double saldoActual;
         
-        miCuenta = new CuentaCorriente("Mariano Martinez","1001-5632-58-234567891",2500,0);
+        cuenta1 = new CuentaCorriente("Mariano Martinez","1001-5632-58-234567891",2500,0);
         try 
         {
-            miCuenta.retirar(-5);
+            cuenta1.retirar(150);
         } catch (Exception e)
         {
             System.err.println(e.getMessage());
@@ -26,13 +26,13 @@ public class Main {
         try
         {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(1695);
+            cuenta1.ingresar(1695);
         } catch (Exception e)
         {
             System.err.println(e.getMessage());
             System.out.println("Fallo al ingresar");
         }
-        saldoActual = miCuenta.estado();
+        saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
     }
     
